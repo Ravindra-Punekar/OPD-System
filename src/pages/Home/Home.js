d4f93957581a6380c651b90d1cdff24e195fd51a
Pramod Daware <dawarepramod4@gmail.com>
2024-03-14 20:06:28 +0530

    home screen

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,24 +4,31 @@ import { getAuth, deleteUser } from "firebase/auth";
 import { Link, useNavigate } from "react-router-dom";
 import NvBar from "../../components/Navbar/Navbar";
 import load from "../../assets/g1.gif";
+import { useState } from "react";
+import { useEffect } from "react";
 
-import "./Home.css"; 
+import "./Home.css";
 
 const auth = getAuth();
 
 const Home = () => {
-  const { logOut, user } = useUserAuth();
-  const navigate = useNavigate();
+    const { logOut, user } = useUserAuth();
+    const navigate = useNavigate();
 
-  return (
-    <div className="profile-container">
-      <NvBar />
-      <div className="welcome-title">WLECOME TO OPD SYSTEM ! </div>
-      <div className="gif-container">
-        <img src={load} alt="Hospital GIF" className="centered-gif" />
-      </div>
+    return (
+        <div className="profile-container">
+            <NvBar />
+            <div className="welcome-slogan">
+      <h1>Your Health is in Good Hands</h1>
+      <span>Streamlined care for your OPD needs.</span>
     </div>
-  );
+            <div className="gif-container">
+                <img src={load} alt="Hospital GIF" className="centered-gif" />
+            </div>
+        </div>
+    );
 };
+
+
 
 export default Home;
